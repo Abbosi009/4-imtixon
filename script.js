4f8082a4ffc64580307d811a29c13d88cf02395d
Abbosi009 <abbosixan@gmail.com>
2025-08-28 18:38:35 +0500

    3 ta masala tugadi

--- script.js
+++ script.js
@@ -6,9 +6,38 @@ let products = [
     {id: 4, name: 'HP', price: 21000, rating: 4.1, discount: 30},
     {id: 5, name: 'DELL', price: 35000, rating: 4.9, discount: 30}
 
-]
-let sortedByRating = [...products].sort((a, b) => b.rating - a.rating)
-let bestProducts = sortByRating[0]
+];
+let sortByRating = [...products].sort((a, b) => b.rating - a.rating);
+let bestProducts = sortByRating[0];
 
-console.log("Rating buyicha saralangan:", sortedByRating);
+console.log("Rating buyicha saralangan:", sortByRating);
 console.log("Eng yuqori ratingga ega product:", bestProducts);
+
+
+//2-masala
+const product = [
+    {id: 1, name: 'Phone'},
+    {id: 2, name: 'Laptop'},
+    {id: 3, name: 'Tablet'},
+    {id: 4, name: 'Smart Watch'},
+];
+function getProdectNameById(id) {
+    const produc = product.find(item => item.id === id);
+    return produc ? product.name : 'Topilmadi';
+}
+
+console.log(getProdectNameById(1));
+console.log(getProdectNameById(2)); 
+console.log(getProdectNameById(3));
+console.log(getProdectNameById(5));
+
+//3-masala
+let arr = [1, 2, 3, 2, 4, 3, 5, 1, 6];
+let iniqueArr = arr.reduce ((acc, curr) => {
+    if (!acc.includes(curr)) {
+        acc.push(curr);
+    }
+    return acc;
+} , []);
+console.log(iniqueArr);
+
